050339821ac06ff80f38878a44b90bf798506380
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-02-10 03:08:02 +0530

    Setup @ngrx/store and @ngrx/store-devtools..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { SimpleSerializedNgRxRouterStoreModule } from 'ss-ngrx-router-store';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './app.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    SimpleSerializedNgRxRouterStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
