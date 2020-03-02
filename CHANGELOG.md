72a8bc988bdea2c8fb02a93be564bbaa0300bccc
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-03-02 23:34:25 +0530

    Built demo with SSR prerendering..

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Changelogs
-
-Check library's [GitHub Releases page](https://github.com/SachinShekhar/ss-ngrx-router-store/releases) for detailed changelogs.
--- projects/ss-ngrx-router-store/src/public-api.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/ss-ngrx-router-store.module';
-export * from './lib/ss-ngrx-router-store.serializer';
-export * from './lib/ss-ngrx-router-store.selectors';
-export * from './lib/ss-ngrx-router-store.models';
